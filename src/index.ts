9122e2e1e900796673723b34c9cf63f42c638d7a
Thomas Schreiber <thomas.schreiber@gmx.at>
2022-12-03 17:05:15 +0100

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+//find binary
+import * as path from "path";
+import {exec} from "child_process";
+
+const which = require('which')
+
+let openpdfsignJar = path.join(__dirname, "open-pdf-sign.jar")
+let javaBinary: string | null = null;
+
+const OpenPdfSign = {
+    async sign(...params: Array<number | string>) {
+        //initialize java binary first
+        if (javaBinary === null) {
+            javaBinary = await which('java', {nothrow: true})
+        }
+
+        if (javaBinary === null) {
+            console.log("open-pdf-sign: no java in PATH")
+            throw "no java in path"
+        }
+
+        let args: Array<number | string> = [javaBinary, "-jar", '"' + openpdfsignJar + '"'];
+        args = args.concat(params)
+        let fullIncovation = args.join(" ");
+
+        //invoke open-pdf-sign
+        await (async () => {
+            return new Promise<void>((resolve, reject) => {
+                exec(fullIncovation, async function (error, stdout, stderr) {
+                    if (error !== null) {
+                        console.log("open-pdf-sign error: " + error);
+                        reject(error)
+                    } else {
+                        console.log("pdf signed")
+                        resolve()
+                    }
+                })
+            })
+        })();
+    }
+}
+
+module.exports = OpenPdfSign;
