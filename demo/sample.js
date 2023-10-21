dfbd38f206be0b3a8cc188aa966368026c234c8f
Thomas Schreiber <thomas.schreiber@gmx.at>
2023-10-21 22:45:40 +0200

    #1 add sample code

--- demo/sample.js
+++ demo/sample.js
@@ -1,4 +1,6 @@
 const openPdfSign = require("../lib")
+const fs = require('fs').promises;
+
 //invoke the opernPdfSign CLI
 //https://github.com/open-pdf-sign/open-pdf-sign
 openPdfSign.sign("-i demo.pdf", "-o demo.signed.pdf","-k key_nopass.pem","-c cert.pem","--page -1")
@@ -8,3 +10,16 @@ openPdfSign.sign("-i demo.pdf", "-o demo.signed.pdf","-k key_nopass.pem","-c cer
 .catch((e) => {
     console.log("error during signature",e)
 })
+
+
+//or use the binary directly, if binary option is given
+openPdfSign.sign("--binary", "-i demo.pdf", "-k key_nopass.pem","-c cert.pem","--page -1")
+    .then((binary) => {
+        return fs.writeFile("demo.signed.binary.pdf", binary);
+    })
+    .then(() => {
+        console.log("signature successful, binary written to file")
+    })
+    .catch((e) => {
+        console.log("error during signature",e)
+    })
